f23f6c82beb442a27da0a571eb040ef60b06f62b
RajendraPrasadVSKall <RajendraPrasadVSKall@DESKTOP-2QOBHK9>
2019-04-30 16:33:49 +0530

    My first commit in rajendra test branch

---
+++ MyFirst/src/com/raj/kalluri/MySecond.java
@@ -0,0 +1,12 @@
+package com.raj.kalluri;
+
+public class MySecond {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("My Second");
+
+	}
+
+}
