1e229714a739e6ce36b48fedbb83ee6596b0993a
RajendraPrasadVSKall <RajendraPrasadVSKall@DESKTOP-2QOBHK9>
2019-04-30 16:51:04 +0530

    My test project

---
+++ testProject/src/com/testProject/TestMain.java
@@ -0,0 +1,12 @@
+package com.testProject;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I am test main");
+
+	}
+
+}
